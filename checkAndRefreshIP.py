39a3515689f0ae696bedfce754d8fb7fb3294964
byBeeMoo <73898144+byBeeMoo@users.noreply.github.com>
2021-09-04 18:02:50 +0200

    Add files via upload

---
+++ checkAndRefreshIP.py
@@ -0,0 +1,38 @@
+import requests as request
+from time import sleep
+import yagmail as mail
+
+lastip = ''
+currentip = ''
+
+user = '{SENDER_MAIL}@gmail.com'
+app_password = '{APP_PASSWD}'
+to = '{RECIEVER_MAIL}@inspedralbes.cat'
+
+subject = 'Your IP has changed!'
+content = []
+
+
+while True:
+    sleep(300)
+    try:
+        response = request.get("https://ifconfig.me/")
+        currentip = response.content.decode(encoding='utf-8')
+    except:
+        print("No gateway to internet found")
+        
+    if lastip != currentip:
+        print(f"CurrentIP: {currentip}\nLastIP: {lastip}")
+        content = [f"Your new ip is: {currentip}"]
+        print(content)
+        lastip = currentip
+        
+        with mail.SMTP(user, app_password) as sendmail:
+            try:
+                sendmail.send(to, subject, content)
+                print("Success")
+            except:
+                print("Mail not sent")
+    else:
+        print("No ip changes")
+    
